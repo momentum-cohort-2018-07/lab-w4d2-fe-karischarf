9f186106b894e9245d4f3494d4d12f08dbbb382e
Kari Scharf <karischarf@gmail.com>
2018-08-22 13:28:39 -0400

    change

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 import request from 'superagent'
 
-request
-  .get('https://api.github.com/users/karischarf')
-  .then(function (response) {
+request.get('https://api.github.com/users/karischarf')
+  .then(response => {
     console.log(response)
+    displayInfo(response.body)
   })
 
-//   let pageContents = 
+function displayInfo (results) {
+  let output = ` 
+  <header>
+  <h1 class="name">${results.name}</h1>
+  </header>
 
-// `
-//   <div>${pageContents.name}</div>
+  <div class="container zigzag">
 
-//  ` 
+  <div class="basics">
+  <h2>basics</h2>
+
+  <ul>
+  <li class="url"> ${results.url} </li>
+  <li class="location"> ${results.location} </li>
+  </ul>
+
+  </div>
+
+  <div class="story"> 
+  <h2 id="story"> story </h2>
+  ${results.bio}</div>
+
+  <img class="photo" src= "${results.avatar_url}">
+
+  </div>
   
+`
+  document.getElementById('output').innerHTML = output
+}
